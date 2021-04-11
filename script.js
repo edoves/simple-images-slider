4388ca96449546ae25163dde9394eeb4729897c2
dove <ndoverte11@gmail.com>
2021-04-11 18:13:35 +0800

    Fixed slider dot navigation current active class indication

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ const dots = Array.from(sliderDotContainer.children);
 dots.forEach((dot, idx) => {
   dot.addEventListener("click", function () {
     slider.style.transform = `translateX(${idx * -25}%)`;
+    sliderDotContainer.querySelector(".active").classList.remove("active");
+    dot.classList.add("active");
   });
 });
